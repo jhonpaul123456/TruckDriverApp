3f888da8773a692a2e09b549a82611682184eef1
jhonpaul123456 <jhonpaul.luceno@hcdc.edu.ph>
2021-02-20 20:17:14 +0800

    Update user done

--- app/src/main/java/com/firstapp/crudappprojectlcnostrd/activities/UpdateUserActivity.java
+++ app/src/main/java/com/firstapp/crudappprojectlcnostrd/activities/UpdateUserActivity.java
@@ -1,5 +1,6 @@
 package com.firstapp.crudappprojectlcnostrd.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,6 +12,7 @@ import com.firstapp.crudappprojectlcnostrd.R;
 import com.firstapp.crudappprojectlcnostrd.databinding.ActivityUpdateUserBinding;
 import com.firstapp.crudappprojectlcnostrd.model.User;
 import com.firstapp.crudappprojectlcnostrd.network.FetchUserService;
+import com.firstapp.crudappprojectlcnostrd.network.UpdateUserService;
 import com.firstapp.crudappprojectlcnostrd.parents.BigPapaActivity;
 
 import retrofit2.Call;
@@ -21,6 +23,7 @@ public class UpdateUserActivity extends BigPapaActivity {
 
     ActivityUpdateUserBinding binding;
     FetchUserService mFetchUserService;
+    UpdateUserService mUpdateUserService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class UpdateUserActivity extends BigPapaActivity {
 
         binding.updateUserBtn.setEnabled(false);
         mFetchUserService = getRetrofit().create(FetchUserService.class);
+        mUpdateUserService = getRetrofit().create(UpdateUserService.class);
 
         binding.id.addTextChangedListener(new TextWatcher() {
             @Override
@@ -47,6 +51,65 @@ public class UpdateUserActivity extends BigPapaActivity {
 
             }
         });
+
+        binding.updateUserBtn.setOnClickListener(v -> {
+            if (clearValidations()) {
+                disableViews(binding.updateUserBtn);
+                mUpdateUserService.updateUser(
+                        binding.id.getText().toString().trim(),
+                        binding.driverName.getText().toString().trim(),
+                        binding.plateNumber.getText().toString().trim(),
+                        binding.truckCondition.getText().toString().trim(),
+                        binding.truckType.getText().toString().trim()
+                ).enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        enableViews(binding.updateUserBtn);
+                        if (response.body().isStatusSuccess()) {
+                            Toast.makeText(UpdateUserActivity.this, "Data Updated !", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(UpdateUserActivity.this, FetchUsersActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(UpdateUserActivity.this, "Something Went Wrong !", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        enableViews(binding.updateUserBtn);
+                        Toast.makeText(UpdateUserActivity.this, "Server Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private boolean clearValidations() {
+        boolean pass = false;
+        if (binding.id.getText().toString().trim().isEmpty()) {
+            binding.id.setError(getString(R.string.please_enter_id));
+            binding.id.requestFocus();
+        } else if (binding.driverName.getText().toString().trim().isEmpty()) {
+            binding.driverName.setError(getString(R.string.please_enter_driver_name));
+            binding.driverName.requestFocus();
+        } else if (binding.plateNumber.getText().toString().trim().isEmpty()) {
+            binding.plateNumber.setError(getString(R.string.please_enter_plate_number));
+            binding.plateNumber.requestFocus();
+        } else if (binding.truckCondition.getText().toString().trim().isEmpty()) {
+            binding.truckCondition.setError(getString(R.string.please_enter_truck_condition));
+            binding.truckCondition.requestFocus();
+        } else if (binding.truckType.getText().toString().trim().isEmpty()) {
+            binding.truckType.setError(getString(R.string.please_enter_truck_type));
+            binding.truckType.requestFocus();
+        } else {
+            binding.id.setError(null);
+            binding.driverName.setError(null);
+            binding.plateNumber.setError(null);
+            binding.truckCondition.setError(null);
+            binding.truckType.setError(null);
+            pass = true;
+        }
+        return pass;
     }
 
     private void fetchAndSetUser(String id) {
@@ -60,6 +123,7 @@ public class UpdateUserActivity extends BigPapaActivity {
                     binding.truckCondition.setText(response.body().getTruckCondition());
                     binding.truckType.setText(response.body().getTruckType());
                 } else {
+                    // todo: this thing need to be fixed.
                     Toast.makeText(UpdateUserActivity.this, "No User Found with id: " + id, Toast.LENGTH_SHORT).show();
                 }
             }
