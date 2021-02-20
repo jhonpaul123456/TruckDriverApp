02923e80cc4cb7ac3d9dd94de2c8f4879f3e2b5e
jhonpaul123456 <jhonpaul.luceno@hcdc.edu.ph>
2021-02-20 14:28:44 +0800

    create mainactivity layout, + create empty CreateUserActivity

---
+++ app/src/main/java/com/firstapp/crudappprojectlcnostrd/activities/CreateUserActivity.java
@@ -0,0 +1,16 @@
+package com.firstapp.crudappprojectlcnostrd.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firstapp.crudappprojectlcnostrd.R;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+}
--- app/src/main/java/com/firstapp/crudappprojectlcnostrd/activities/MainActivity.java
+++ app/src/main/java/com/firstapp/crudappprojectlcnostrd/activities/MainActivity.java
@@ -3,14 +3,35 @@ package com.firstapp.crudappprojectlcnostrd.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.firstapp.crudappprojectlcnostrd.R;
+import com.firstapp.crudappprojectlcnostrd.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.createBtn.setOnClickListener(v -> {
+
+        });
+
+        binding.deleteUser.setOnClickListener(v -> {
+
+        });
+
+        binding.fetchUser.setOnClickListener(v -> {
+
+        });
+
+        binding.updateUser.setOnClickListener(v -> {
+
+        });
+
     }
 }
